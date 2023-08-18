95b053f519f32d6379b5ccfa602e106030b9b485
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-08-18 16:47:28 +0200

    still not working with nvim + eslint + prettier

--- src/components/Comment/Comment.stories.tsx
+++ src/components/Comment/Comment.stories.tsx
@@ -1,19 +1,19 @@
-import { View } from "react-native";
-import { Comment } from "./index";
-import { ThemeProvider } from "styled-components/native";
-import { darkTheme, lightTheme } from "themes";
-import { ComponentStory, ComponentMeta } from "@storybook/react-native";
+import { View } from 'react-native';
+import { Comment } from './index';
+import { ThemeProvider } from 'styled-components/native';
+import { darkTheme, lightTheme } from 'themes';
+import { ComponentStory, ComponentMeta } from '@storybook/react-native';
 
 const CommentMeta: ComponentMeta<typeof Comment> = {
-    title: "Components/Comment",
+    title: 'Components/Comment',
     component: Comment,
     args: {},
     decorators: [
-        (Story) => (
+        Story => (
             <View style={{ padding: 16 }}>
                 <Story />
             </View>
-        )
+        ),
     ],
 };
 
@@ -21,31 +21,30 @@ export default CommentMeta;
 
 type MyCommentStory = ComponentStory<typeof Comment>;
 
-export const Light: MyCommentStory = (args) => {
+export const Light: MyCommentStory = args => {
     return (
         <ThemeProvider theme={lightTheme}>
             <Comment {...args} />
         </ThemeProvider>
-    )
-}
+    );
+};
 
-export const Dark: MyCommentStory = (args) => {
+export const Dark: MyCommentStory = args => {
     return (
         <ThemeProvider theme={darkTheme}>
             <Comment {...args} />
         </ThemeProvider>
-    )
-}
+    );
+};
 
 Light.parameters = {
     backgrounds: {
-        default: 'light'
-    }
-}
+        default: 'light',
+    },
+};
 
 Dark.parameters = {
     backgrounds: {
-        default: 'dark'
-    }
-}
-
+        default: 'dark',
+    },
+};
