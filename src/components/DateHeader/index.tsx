74daca446f730c696543ba3335599971220298d4
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-12-21 00:25:45 +0700

    Add ProgressBar Component on DateHeader

--- src/components/DateHeader/index.tsx
+++ src/components/DateHeader/index.tsx
@@ -1,6 +1,8 @@
-import { useTranslation } from 'react-i18next';
+import styled from 'styled-components/native';
 import { View } from 'react-native';
 import { months, days } from 'utils/constants';
+import { useTranslation } from 'react-i18next';
+import Svg, { Circle, Rect } from 'react-native-svg';
 import { Container, DateLabelText, MonthLabelText } from './styled';
 
 interface IDateLabel {
@@ -23,9 +25,43 @@ interface IProgressCircle {
 }
 export const ProgressCircle = (props: IProgressCircle) => {
     const { progress } = props;
+    const size = 65;
+    const strokeWidth = 13;
+    const radius = (size - strokeWidth) / 2;
+    const circum = radius * 2 * Math.PI;
+    const svgProgress = 100 - progress;
+
+    const CircleStyled = styled(Circle)`
+        fill: transparent;
+        stroke: ${props => props.theme.gray};
+    `;
+
+    const CircleProgressStyled = styled(Circle)`
+        fill: transparent;
+        stroke: ${props => props.theme.txtColor};
+    `;
+
     return (
-        <View>
-            <DateLabelText children={progress} />
+        <View style={{ bottom: 10 }}>
+            <Svg width={size} height={size}>
+                <CircleStyled
+                    r={radius}
+                    cx={size / 2}
+                    cy={size / 2}
+                    strokeWidth={strokeWidth}
+                />
+                <CircleProgressStyled
+                    r={radius}
+                    cx={size / 2}
+                    cy={size / 2}
+                    strokeWidth={strokeWidth}
+                    strokeDasharray={`${circum} ${circum}`}
+                    transform={`rotate(-90, ${size / 2}, ${size / 2})`}
+                    strokeDashoffset={
+                        radius * Math.PI * 2 * (svgProgress / 100)
+                    }
+                />
+            </Svg>
         </View>
     );
 };
