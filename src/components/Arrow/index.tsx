e23312e4b04734d208bfcc2cf3be0a92d9ad56bc
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-12-24 12:44:31 +0700

    Add Arrow Component Styled

--- src/components/Arrow/index.tsx
+++ src/components/Arrow/index.tsx
@@ -1,19 +1,13 @@
 import { useTheme } from 'styled-components/native';
-import { fontSizes, Direction } from 'utils/constants';
-import styled from 'styled-components/native';
+import { ImageStyled } from './styled';
 
 export const Arrow = (props: { direction: Direction }) => {
     const { direction } = props;
     const { themeName } = useTheme();
 
-    const ImageStyled = styled.Image`
-        height: ${fontSizes.large}px;
-        width: ${fontSizes.large}px;
-        transform: rotate(${direction === Direction.LEFT ? '180deg' : '0deg'});
-    `;
-
     return (
         <ImageStyled
+            direction={direction}
             source={
                 themeName === 'darkTheme'
                     ? require('../../../assets/icons/white_arrow.png')
---
+++ src/components/Arrow/styled.tsx
@@ -0,0 +1,10 @@
+import { fontSizes, Direction } from 'utils/constants';
+import styled from 'styled-components/native';
+
+export const ImageStyled = styled.Image<{ direction: string }>`
+    height: ${fontSizes.large}px;
+    width: ${fontSizes.large}px;
+    transform: rotate(
+        ${props => (props.direction === Direction.LEFT ? '180deg' : '0deg')}
+    );
+`;
