e3e107cb28b7f0951f7263bdc237af05d7701292
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-12-25 20:40:55 +0700

    Add Habit >> Create >> Show + Buttons

--- src/components/Habit/index.tsx
+++ src/components/Habit/index.tsx
@@ -1,42 +1,115 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
-    AddHabitLabel,
-    NewHabitInput,
-    HabitContainer,
-    AddHabitContainer,
-    CreateHabitContainer,
-    HabitButtonsContainer,
+    ButtonLabel,
     CancelButton,
     CreateButton,
-    TextButton,
-    CreateText,
+    AddHabitLabel,
+    HabitContainer,
+    ShowHabitLabel,
+    AddHabitContainer,
+    CreateButtonLabel,
+    ShowHabitContainer,
+    CreateNewHabitInput,
+    CreateHabitContainer,
+    CreateButtonsContainer,
+    ShowHabitButtonContainer,
+    ShowHabitIndicator,
 } from './styled';
 import _ from 'lodash';
+import styled from 'styled-components/native';
+import { verticalScale } from 'utils/scaleFunctions';
 
-export const CreateHabit = (props: { onCancel: Function }) => {
-    const { onCancel } = props;
-    const [value, setValue] = useState<string>('');
+const ShowHabit = (props: {
+    habit: string;
+    isDone: boolean;
+    setIsDone: (done: boolean) => void;
+}) => {
+    const { habit, isDone, setIsDone } = props;
+
+    const CheckButton = styled.TouchableOpacity`
+        height: 80%;
+        width: ${verticalScale(80)}px;
+        justify-content: center;
+        border-radius: 7px;
+        border: 2px solid ${props => props.theme.borderColor};
+        background-color: ${props => props.theme.gray};
+    `;
+
+    const CheckIndicator = styled.View<{ isDone: boolean }>`
+        height: 100%;
+        width: ${verticalScale(40)}px;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        border-right-width: 2px;
+        border-right-style: solid;
+        border-right-color: ${props =>
+            props.isDone ? props.theme.borderColor : props.theme.selected};
+        background-color: ${props =>
+            props.isDone ? props.theme.selected : props.theme.bgColor};
+        transform: translateX(
+            ${props => (props.isDone ? verticalScale(39) : 0)}px
+        );
+    `;
+
+    const OptionsButton = styled.TouchableOpacity`
+        width: 20%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    `;
+
+    return (
+        <ShowHabitContainer>
+            <ShowHabitLabel children={habit} />
+            <ShowHabitButtonContainer>
+                <CheckButton onPress={() => setIsDone(!isDone)}>
+                    <CheckIndicator isDone={isDone}>
+                        <ShowHabitIndicator
+                            isDone={isDone}
+                            children={isDone ? '✔' : '○'}
+                        />
+                    </CheckIndicator>
+                </CheckButton>
+                <OptionsButton children={<ButtonLabel children={':'} />} />
+            </ShowHabitButtonContainer>
+        </ShowHabitContainer>
+    );
+};
+
+export const CreateHabit = (props: {
+    habit: string;
+    setHabit: Function;
+    setMode: (mode: EHabit) => void;
+}) => {
+    const { habit, setHabit, setMode } = props;
     return (
         <CreateHabitContainer>
-            <NewHabitInput
-                value={value}
+            <CreateNewHabitInput
+                value={habit}
                 autoFocus={true}
-                placeholder={'new habit'}
+                placeholder={_.isEmpty(habit) ? 'new habit' : habit}
                 placeholderTextColor={'gray'}
-                onChangeText={txt => setValue(txt)}
+                onChangeText={txt => setHabit(txt)}
             />
-            <HabitButtonsContainer>
+            <CreateButtonsContainer>
                 <CancelButton
-                    onPress={() => onCancel()}
-                    children={<TextButton children={'x'} />}
+                    onPress={() => {
+                        setHabit('');
+                        setMode(EHabit.ADD);
+                    }}
+                    children={<ButtonLabel children={'x'} />}
                 />
                 <CreateButton
-                    value={value}
-                    disabled={_.isEmpty(value)}
-                    children={<CreateText value={value} children={'✔'} />}
+                    onPress={() => setMode(EHabit.SHOW)}
+                    value={habit}
+                    disabled={_.isEmpty(habit)}
+                    children={
+                        <CreateButtonLabel value={habit} children={'✔'} />
+                    }
                 />
-            </HabitButtonsContainer>
+            </CreateButtonsContainer>
         </CreateHabitContainer>
     );
 };
@@ -44,11 +117,16 @@ export const CreateHabit = (props: { onCancel: Function }) => {
 enum EHabit {
     ADD = 'add',
     CREATE = 'create',
+    SHOW = 'show',
     EDIT = 'edit',
 }
 export const Habit = (props: {}) => {
     const { t } = useTranslation();
-    const [mode, setMode] = useState<EHabit>(EHabit.ADD);
+    const [habit, setHabit] = useState<string>('');
+    const [isDone, setIsDone] = useState<boolean>(false);
+    const [mode, setMode] = useState<EHabit>(
+        _.isEmpty(habit) ? EHabit.ADD : EHabit.SHOW
+    );
 
     return (
         <HabitContainer>
@@ -58,8 +136,21 @@ export const Habit = (props: {}) => {
                     children={<AddHabitLabel children={t('addHabit')} />}
                 />
             )}
+
             {mode === EHabit.CREATE && (
-                <CreateHabit onCancel={() => setMode(EHabit.ADD)} />
+                <CreateHabit
+                    habit={habit}
+                    setMode={setMode}
+                    setHabit={setHabit}
+                />
+            )}
+
+            {mode === EHabit.SHOW && (
+                <ShowHabit
+                    habit={habit}
+                    isDone={isDone}
+                    setIsDone={setIsDone}
+                />
             )}
         </HabitContainer>
     );
--- src/components/Habit/styled.tsx
+++ src/components/Habit/styled.tsx
@@ -3,45 +3,37 @@ import { verticalScale } from 'utils/scaleFunctions';
 import { fontSizes, fontFamilies } from 'utils/constants';
 import _ from 'lodash';
 
-export const TextButton = styled.Text`
-    color: ${props => props.theme.txtColor};
-    font-family: ${fontFamilies.CascadiaBold};
-    font-size: ${fontSizes.large + verticalScale(2)}px;
-`;
-
-export const CancelButton = styled.TouchableOpacity`
-    height: 80%;
-    width: ${verticalScale(50)}px;
-    align-items: center;
-    justify-content: center;
-    border-radius: 10px;
+export const HabitContainer = styled.View`
+    width: 100%;
+    height: ${verticalScale(40)}px;
+    border-radius: 7px;
     border: 2px solid ${props => props.theme.borderColor};
+    padding-left: ${verticalScale(15)}px;
+    padding-right: ${verticalScale(15)}px;
 `;
 
-export const CreateText = styled(TextButton)<{ value: string }>`
-    color: ${props =>
-        _.isEmpty(props.value) ? props.theme.gray : props.theme.txtSelected};
+export const AddHabitContainer = styled.TouchableOpacity`
+    width: 100%;
+    height: ${verticalScale(40)}px;
+    justify-content: center;
+    align-items: center;
 `;
 
-export const CreateButton = styled(CancelButton)<{ value: string }>`
-    border: 2px solid
-        ${props =>
-            _.isEmpty(props.value)
-                ? props.theme.gray
-                : props.theme.borderColor};
-    background-color: ${props =>
-        _.isEmpty(props.value) ? props.theme.bgColor : props.theme.borderColor};
+export const AddHabitLabel = styled.Text`
+    font-size: ${fontSizes.large}px;
+    font-family: ${fontFamilies.CascadiaItalic};
+    color: ${props => props.theme.txtColor};
 `;
 
-export const HabitButtonsContainer = styled.View`
+export const CreateHabitContainer = styled.View`
+    width: 100%;
     height: 100%;
-    width: ${verticalScale(120)}px;
     flex-direction: row;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
 `;
 
-export const NewHabitInput = styled.TextInput<{ value: string }>`
+export const CreateNewHabitInput = styled.TextInput<{ value: string }>`
     width: 70%;
     height: 100%;
     color: ${props => props.theme.txtColor};
@@ -54,32 +46,51 @@ export const NewHabitInput = styled.TextInput<{ value: string }>`
     outline-style: none;
 `;
 
-export const CreateHabitContainer = styled.View`
-    width: 100%;
+export const CreateButtonsContainer = styled.View`
     height: 100%;
+    width: ${verticalScale(120)}px;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-around;
 `;
 
-export const AddHabitLabel = styled.Text`
-    font-size: ${fontSizes.large}px;
-    font-family: ${fontFamilies.CascadiaItalic};
-    color: ${props => props.theme.txtColor};
-`;
-
-export const AddHabitContainer = styled.TouchableOpacity`
-    width: 100%;
-    height: ${verticalScale(40)}px;
-    justify-content: center;
+export const CancelButton = styled.TouchableOpacity`
+    height: 80%;
+    width: ${verticalScale(50)}px;
     align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    border: 2px solid ${props => props.theme.borderColor};
 `;
 
-export const HabitContainer = styled.View`
-    width: 100%;
-    height: ${verticalScale(40)}px;
-    border-radius: 7px;
-    border: 2px solid ${props => props.theme.borderColor};
-    padding-left: ${verticalScale(15)}px;
-    padding-right: ${verticalScale(15)}px;
+export const CreateButton = styled(CancelButton)<{ value: string }>`
+    border: 2px solid
+        ${props =>
+            _.isEmpty(props.value)
+                ? props.theme.gray
+                : props.theme.borderColor};
+    background-color: ${props =>
+        _.isEmpty(props.value) ? props.theme.bgColor : props.theme.borderColor};
+`;
+
+export const ButtonLabel = styled.Text`
+    color: ${props => props.theme.txtColor};
+    font-family: ${fontFamilies.CascadiaBold};
+    font-size: ${fontSizes.large + verticalScale(2)}px;
+`;
+
+export const CreateButtonLabel = styled(ButtonLabel)<{ value: string }>`
+    color: ${props =>
+        _.isEmpty(props.value) ? props.theme.gray : props.theme.txtSelected};
+`;
+
+export const ShowHabitContainer = styled(CreateHabitContainer)``;
+
+export const ShowHabitLabel = styled(ButtonLabel)``;
+
+export const ShowHabitButtonContainer = styled(CreateButtonsContainer)``;
+
+export const ShowHabitIndicator = styled(ButtonLabel)<{ isDone: boolean }>`
+    color: ${props =>
+        props.isDone ? props.theme.txtSelected : props.theme.gray};
 `;
