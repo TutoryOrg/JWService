098ef2b2b3008be442b98d75e40b5eb12ddcdac3
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2024-01-29 22:40:09 +0700

    Clean Code + Today Screen Finished :fire:

--- src/store/redux/habits/index.ts
+++ src/store/redux/habits/index.ts
@@ -1,4 +1,3 @@
-import { isSameDay } from 'utils/scaleFunctions';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface IHabit {
@@ -19,13 +18,6 @@ export interface ISavedHabits {
     savedHabits: IStoreHabits[];
 }
 
-const findIndexByDate = (savedHabits: IStoreHabits[], date: string) => {
-    return savedHabits.findIndex(
-        ({ date: savedDate }) =>
-            savedDate && isSameDay(new Date(savedDate), new Date(date))
-    );
-};
-
 const initialState: ISavedHabits = {
     savedHabits: [],
 };
@@ -35,88 +27,10 @@ const habitsSlice = createSlice({
     initialState,
     reducers: {
         setSavedHabits(state, action: PayloadAction<IStoreHabits[]>) {
-            console.log('saved');
             state.savedHabits = action.payload;
-        },
-        saveHabits(
-            state,
-            action: PayloadAction<{ date: string; habits: IHabit[] }>
-        ) {
-            const indexDay = findIndexByDate(
-                state.savedHabits,
-                action.payload.date
-            );
-
-            if (indexDay === -1) {
-                state.savedHabits = [
-                    {
-                        date: action.payload.date,
-                        habits: action.payload.habits,
-                        image: '',
-                        description: '',
-                        progress: 0,
-                    },
-                    ...state.savedHabits,
-                ];
-            } else {
-                state.savedHabits[indexDay].habits = action.payload.habits;
-            }
-        },
-        saveImage(
-            state,
-            action: PayloadAction<{ date: string; image: string }>
-        ) {
-            const indexDay = findIndexByDate(
-                state.savedHabits,
-                action.payload.date
-            );
-
-            if (indexDay === -1) {
-                state.savedHabits = [
-                    {
-                        date: action.payload.date,
-                        habits: [],
-                        image: action.payload.image,
-                        description: '',
-                        progress: 0,
-                    },
-                    ...state.savedHabits,
-                ];
-            } else {
-                state.savedHabits[indexDay].image = action.payload.image;
-            }
-        },
-        saveDesc(
-            state,
-            action: PayloadAction<{ date: string; description: string }>
-        ) {
-            const indexDay = findIndexByDate(
-                state.savedHabits,
-                action.payload.date
-            );
-
-            if (indexDay === -1) {
-                state.savedHabits = [
-                    {
-                        date: action.payload.date,
-                        habits: [],
-                        image: '',
-                        description: action.payload.description,
-                        progress: 0,
-                    },
-                    ...state.savedHabits,
-                ];
-            } else {
-                state.savedHabits[indexDay].description =
-                    action.payload.description;
-            }
-        },
-        saveProgress(state, action: PayloadAction<{ progress: number }>) {
-            state.savedHabits[0].progress = action.payload.progress;
         },
     },
 });
 
-export const { saveHabits, saveImage, saveDesc, saveProgress, setSavedHabits } =
-    habitsSlice.actions;
+export const { setSavedHabits } = habitsSlice.actions;
 export const habitsReducer = habitsSlice.reducer;
