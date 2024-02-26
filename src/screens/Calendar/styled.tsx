0b73c771a21c0a5f8eaab4b8fff7c557560dad79
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2024-02-26 11:26:55 +0700

    cc

--- src/screens/Calendar/styled.tsx
+++ src/screens/Calendar/styled.tsx
@@ -4,13 +4,11 @@ import { windowHeight, windowWidth } from 'utils/scaleFunctions';
 export const ItemContainer = styled.View`
     width: ${windowWidth - 100}px;
     height: 100%;
-    background-color: red;
 `;
 
 export const CalendarContentContainer = styled.View`
     width: ${windowWidth - 100}px;
     height: 90%;
-    background-color: yellow;
     align-self: center;
 `;
 
