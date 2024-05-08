6a40c1ec8d7156bf3bd843c1e8bf38d23593da1e
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2024-05-08 17:22:01 +0700

    cc translations

--- src/screens/Calendar/YearContent/index.tsx
+++ src/screens/Calendar/YearContent/index.tsx
@@ -1,18 +1,20 @@
 import { months } from 'utils/constants';
 import { MonthContainer, MonthLabel, YearContentContainer } from './styled';
+import { useTranslation } from 'react-i18next';
 
 interface IYearContent {
     date: Date;
 }
 export const YearContent = (props: IYearContent) => {
     const { date } = props;
+    const { t } = useTranslation();
     const month = months[date.getMonth()];
     console.log({ month });
     return (
         <YearContentContainer>
             {months.map(m => (
                 <MonthContainer>
-                    <MonthLabel>{m}</MonthLabel>
+                    <MonthLabel>{t(`${m}_`)}</MonthLabel>
                 </MonthContainer>
             ))}
         </YearContentContainer>
