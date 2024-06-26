536bad1a866aa5e322355cde83e3d64c1bc38394
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2024-06-26 11:38:03 +0700

    cc

--- src/components/ImagePicker/index.tsx
+++ src/components/ImagePicker/index.tsx
@@ -1,17 +1,18 @@
 import { useAtom } from 'jotai';
 import { contentAtom, modalAtom } from 'navigation/Menu';
-import { useRef } from 'react';
-import { isMobile } from 'utils/scaleFunctions';
+import { useRef, useState } from 'react';
 import { CommentDesc, ImageViewer, ImageContainer, ImagePickerContainer, OptionsContainer } from './styled';
 import {
     launchCameraAsync,
     useCameraPermissions,
     launchImageLibraryAsync,
 } from 'expo-image-picker';
-import _ from 'lodash';
-import { Modal } from 'components';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { useTheme } from 'styled-components/native';
+import _ from 'lodash';
+import { Camera } from 'expo-camera';
+import { isMobile } from 'utils/scaleFunctions';
+
 
 interface IImagePicker {
     image: string;
@@ -23,55 +24,68 @@ interface IImagePicker {
 
 export const ImagePicker = ({ desc, image, editable, onChangeImage, onAddDesc }: IImagePicker) => {
     const [status, requestPermission] = useCameraPermissions();
-    const [isOpen, setOpen] = useAtom(modalAtom);
-    const [content, setContent] = useAtom(contentAtom);
     const { themeName } = useTheme();
 
-    if (!status?.granted) requestPermission();
+    const [isOpen, setOpen] = useAtom(modalAtom);
+    const [content, setContent] = useAtom(contentAtom);
+    const [option, setOption] = useState<string | undefined>(undefined)
 
-    const pickImageAsync = async () => {
+    if (!status?.granted) {
+        Camera.requestCameraPermissionsAsync()
+        requestPermission();
+    }
+
+    const openOptions = () => {
         if (!status?.granted) return;
         if (!editable) return;
         setOpen(true)
         setContent(OptionsModal())
 
+    }
 
-        // let result = isMobile
-        //     ? await launchCameraAsync({
-        //           allowsEditing: true,
-        //           quality: 1,
-        //       })
-        //     : await launchImageLibraryAsync({
-        //           allowsEditing: true,
-        //           quality: 1,
-        //       });
-        // if (result.assets) onChangeImage(result.assets[0].uri);
+    const pickImageAsync = async (option: string) => {
+        console.log({ option })
+        console.log(_.isEqual(option, 'camera'))
+
+        let result = option === 'camera'
+            ? await launchCameraAsync({
+                allowsEditing: true,
+                quality: 1,
+            })
+            : await launchImageLibraryAsync({
+                allowsEditing: true,
+                quality: 1,
+            });
+        if (result.assets) { onChangeImage(result.assets[0].uri); setOpen(false) }
     };
+
 
     const OptionsModal = () => {
         return (
             <OptionsContainer>
-                <TouchableOpacity>
+                {/*<Camera style={{ height: 100, width: 100 }} />*/}
+                <TouchableOpacity onPress={() => pickImageAsync('gallery')}>
                     <Image source={
                         themeName === 'darkTheme'
                             ? require('../../../assets/icons/black_gallery.png')
                             : require('../../../assets/icons/white_gallery.png')
                     } />
                 </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => pickImageAsync('camera')}>
                     <Image source={
                         themeName === 'darkTheme'
                             ? require('../../../assets/icons/black_camera.png')
                             : require('../../../assets/icons/white_camera.png')
                     } />
                 </TouchableOpacity>
+
             </OptionsContainer>
         )
     }
 
     return (
         <ImagePickerContainer>
-            <ImageContainer disabled={!editable} onPress={pickImageAsync}>
+            <ImageContainer disabled={!editable} onPress={openOptions}>
                 {!_.isEmpty(image) && <ImageViewer resizeMode={'cover'} source={{ uri: image }} />}
             </ImageContainer>
             <CommentDesc value={desc} editable={editable} onChangeText={onAddDesc} maxLength={25} />
