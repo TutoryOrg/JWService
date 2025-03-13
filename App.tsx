9d20f973f3a5e7f25c888e2dc404dd040e3cc36e
Alejandro Alcocer <saavalencia97@gmail.com>
2025-03-13 12:27:48 +0700

    refactor: format code for consistency and readability

--- App.tsx
+++ App.tsx
@@ -11,28 +11,28 @@ import { darkTheme, lightTheme } from 'themes';
 import Constants from 'expo-constants';
 
 function App() {
-    const store = useAppState();
-    const isDarkMode = useColorScheme() === 'dark';
-    const { fontsLoaded, onLayoutRootView } = useFontsAndLayout();
+	const store = useAppState();
+	const isDarkMode = useColorScheme() === 'dark';
+	const { fontsLoaded, onLayoutRootView } = useFontsAndLayout();
 
-    if (!fontsLoaded) return null;
+	if (!fontsLoaded) return null;
 
-    return (
-        <Provider store={store}>
-            <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-                <Menu onLayoutRootView={onLayoutRootView} />
-                <StatusBar style={isDarkMode ? 'light' : 'dark'} />
-            </ThemeProvider>
-        </Provider>
-    );
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+				<Menu onLayoutRootView={onLayoutRootView} />
+				<StatusBar style={isDarkMode ? 'light' : 'dark'} />
+			</ThemeProvider>
+		</Provider>
+	);
 }
 
 let AppEntryPoint = App;
 
 if (Constants.expoConfig?.extra?.storybookEnabled === 'true') {
-    AppEntryPoint = require('./.ondevice').default;
+	AppEntryPoint = require('./.ondevice').default;
 } else {
-    SplashScreen.preventAutoHideAsync();
+	SplashScreen.preventAutoHideAsync();
 }
 
 export default AppEntryPoint;
--- src/store/async/storeData.ts
+++ src/store/async/storeData.ts
@@ -1,11 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const storeData = async (key: string, value: string): Promise<void | boolean> => {
-    try {
-        await AsyncStorage.setItem(key, value);
-        return true;
-    } catch (e) {
-        // saving error
-        console.error(e);
-    }
+	try {
+		await AsyncStorage.setItem(key, value);
+		return true;
+	} catch (e) {
+		// saving error
+		console.error(e);
+	}
 };
